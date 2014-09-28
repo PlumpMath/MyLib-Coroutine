ba8b8a3b53841577d518910286772655886be674
hassyX <xmyinfo@gmail.com>
2014-09-28 16:01:14 +0900

    プロジェクトを追加

---
+++ MyCoroutine-02/Assets/Library/BehaviourData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MyLib
+{
+	public class BehaviourData
+	{
+		public MyLib.MonoBehaviour behaviour;
+		public bool mainloopBegan;
+		public LinkedList<IEnumerator> routineList;
+
+		public BehaviourData(MyLib.MonoBehaviour behaviour)
+		{
+			this.behaviour = behaviour;
+			this.mainloopBegan = false;
+			this.routineList = new LinkedList<IEnumerator>();
+		}
+	}
+}
---
+++ MyCoroutine-02/Assets/Test/Test.cs
@@ -0,0 +1,38 @@
+﻿using MyLib;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+	public override void Start()
+	{
+		// イテレータ指定で起動
+		StartCoroutine(RoutineTest("A"));
+		// メソッド名指定で起動
+		StartCoroutine("RoutineTest", "B");
+	}
+
+	int updateCount;
+
+	public override void Update()
+	{
+		UnityEngine.Debug.Log("Update:" + updateCount);
+		++updateCount;
+	}
+
+	private IEnumerator RoutineTest(string name)
+	{
+		int count = 0;
+
+		while (true)
+		{
+			if (count >= 100)
+			{
+				yield break;
+			}
+
+			++count;
+			UnityEngine.Debug.Log("★" + name + ", " + count);
+			yield return null;
+		}
+	}
+}
